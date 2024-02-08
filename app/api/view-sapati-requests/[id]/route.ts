3dc5ba0ae5f466c02e2050f8d28d64293d8234cd
puru12 <purushottam.makura@gmail.com>
2024-02-08 17:36:54 +0545

    add:apis

--- app/api/view-sapati-requests/[id]/route.ts
+++ app/api/view-sapati-requests/[id]/route.ts
@@ -25,7 +25,9 @@ return NextResponse.json({message:"Successfully fetched sapati",sapati},{status:
 export async function PATCH(req:NextRequest,params:any){
     try {
 
-        const {request_status}=await req.json();
+        const {request_status,decline_reason,change_reason}=await req.json();
+
+        console.log(request_status);
 
        
         const id=params.params.id;
@@ -42,9 +44,40 @@ if(request_status==="APPROVED"){
             id:sapati.id
         },
         data:{
-            
+            request_status:"APPROVED"
         }
     })
+
+    return NextResponse.json({message:"Successfully approved sapati",},{status:200})
+}else if(request_status==="REJECTED"){
+    await prismadb.sapatiRequests.update({
+        where:{
+            id:sapati.id
+        },
+        data:{
+            request_status:"REJECTED",
+            decline_reason
+        }
+    });
+    return NextResponse.json({message:"Successfully rejected sapati",},{status:200})
+
+
+}else if(request_status==="CHANGE"){
+    await prismadb.sapatiRequests.update({
+        where:{
+            id:sapati.id
+        },
+        data:{
+            request_status:"CHANGE",
+            change_reason
+        }
+    });
+    return NextResponse.json({message:"Successfully requested to change sapati",},{status:200})
+
+
+}else{
+    return NextResponse.json({message:"Please choose one method ie:Pending, Approved Declined or Change",},{status:401})
+
 }
         
     } catch (error) {
