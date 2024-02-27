656000f415fae7a0d65b1fe701a2615a643d08b0
puru12 <purushottam.makura@gmail.com>
2024-02-27 12:40:44 +0545

    api udate

--- app/api/get-transactions-by-user/[id]/route.ts
+++ app/api/get-transactions-by-user/[id]/route.ts
@@ -19,113 +19,197 @@ export async function GET(req:NextRequest,params:any){
         const bearerToken = token.split(" ")[1];
         const existingToken = await verifyBearerToken(bearerToken);
         if (!existingToken) {
-          return NextResponse.json({ message: "Invalid token" }, { status: 400 });
+          return NextResponse.json({ message: "Invalid token" }, { status: 498 });
         }
-        const pgnum: any = req.nextUrl.searchParams.get("pgnum") ?? 0;
-        const pgsize: number = 10;
-        const status=req.nextUrl.searchParams.get("status");
-
-
-        const user = await prismadb.user.findUnique({
-          where: {
-            id
-          },
-          include: {
-            borrowings: {
-              include: {
-                sapati: true
-              },
-              skip: (pgnum ) * pgsize,
-              take: pgsize,
-              orderBy: {
-                created_at:"desc"
-
-              },
-            
-            },
-            lendings: {
-              include: {
-                sapati: true
-              },
-              skip: (pgnum ) * pgsize,
-              take: pgsize,
-              orderBy: {
-                created_at:"desc"
-              },
-             
-            }
-          }
-        });
+    
+        const user = await getUserById(existingToken.user_id);
         if (!user) {
-            return NextResponse.json({ message: "No user found" }, { status: 404 });
-        }
-        let borrowingsForMe=user.lendings.filter((item)=>((item.sapati.created_by==user.id&&item.sapati.created_for==existingToken.user_id)||(item.sapati.created_by==existingToken.user_id&&item.sapati.created_for==user.id)))
-
-    
-
-        //    getting borrowings from user
-        //  user borrowings is lendings for me 
-           let lendingsForMe=user.borrowings.filter((item)=>((item.sapati.created_by==user.id&&item.sapati.created_for==existingToken.user_id)||(item.sapati.created_by==existingToken.user_id&&item.sapati.created_for==user.id)))
-
+          return NextResponse.json({ message: "No user found" }, { status: 404 });
           
-
-           const rejectedBorrowings=borrowingsForMe.filter((item)=>(item.sapati.sapati_satatus=="DECLINED"))
-           const settledBorrowings=borrowingsForMe.filter((item)=>(item.sapati.confirm_settlement))
-           const activeBorrowings=borrowingsForMe.filter((item)=>(!item.sapati.confirm_settlement&&item.sapati.sapati_satatus!=="DECLINED"&&item.sapati.sapati_satatus!=="CHANGE"))
-           const rejectedLendings=lendingsForMe.filter((item)=>(item.sapati.sapati_satatus=="DECLINED"))
-           const settledLendings=lendingsForMe.filter((item)=>(item.sapati.confirm_settlement))
-           const activeLendings=lendingsForMe.filter((item)=>(!item.sapati.confirm_settlement&&item.sapati.sapati_satatus!=="DECLINED"&&item.sapati.sapati_satatus!=="CHANGE"))
-           let rejected=[...rejectedBorrowings,...rejectedLendings]
-           let settled=[...settledBorrowings,...settledLendings]
-           let active=[...activeBorrowings,...activeLendings]
-           const activeBook=[]
-           console.log(settled)
-           console.log(active)
-           console.log(rejected)
-
-     for (const item of active) {
-      const createbBy=item.sapati.created_by
-      const receivedBy=item.sapati.created_for
-      const creatorUser=await getUserById(createbBy!)
-      const receiverUser=await getUserById(receivedBy!)
-
-      console.log(receiverUser)
-      console.log(creatorUser)
-      console.log(createbBy,receivedBy);
-      
-      const remarks=item.sapati.remarks;
-      const amount=item.sapati.amount;
-      const type=item.sapati.type=="BORROWED"?"LENDED":"BORROWED"
-      console.log(type)
-      const returnDate=item.sapati.return_date     
-      const paymentDate=item.sapati.taken_date
-      // const isVerified=
-
-     }
-
-
-           if(status=="settled"){
-           
-            return NextResponse.json({message:"Successfully fetched transactions",data:settled},{status:200})
-          }
-    
-          if(status=="rejected"){
-           
-            return NextResponse.json({message:"Successfully fetched transactions",data:rejected},{status:200})
-          }
-
-          if(status=="activebook"){
-            return NextResponse.json({message:"Successfully fetched transactions of user",data:active},{status:200})
-
-          }
-           return NextResponse.json({message:"yes",},{status:200})
+        }
 
         
-       
+        const status=req.nextUrl.searchParams.get("status");
+        let data;
+        const findUser=await getUserById(id);
+
+
+        if(!findUser){
+            return NextResponse.json({message:"No user found"},{status:404})
+        }
+
+        
+        const pgnum: any = req.nextUrl.searchParams.get("pgnum") ?? 0;
+        const pgsize: number = 20;
+
+        let borrowings=await prismadb.borrowings.findMany({
+          skip: parseInt(pgnum) * pgsize,
+          take: pgsize,
+            where:{
+                user_id:findUser.id,
+            },
+            include:{
+                sapati:true,
+                user:true,
+            }
+        });
+
+        
+        let lendings=await prismadb.lendings.findMany({
+          skip: parseInt(pgnum) * pgsize,
+          take: pgsize,
+            where:{
+                user_id:findUser.id
+            },
+            include:{
+                sapati:true,
+                user:true,
+            }
+        });
+
+        console.log(id)
+        console.log(existingToken.user_id)
+        console.log(borrowings);
+        console.log(lendings);
+     
+
+        borrowings=borrowings.filter((item)=>((item.sapati.created_by==id||item.sapati.created_for==existingToken.user_id)&&(item.sapati.created_by==existingToken.user_id||item.sapati.created_for==id)))
+        console.log(borrowings);
+
+        for (const item of borrowings) {
+          console.log(item);
+          const phone = parseInt(item.sapati.phone);
+          if (!isNaN(phone)) {
+            const borrower_user = await getUserByPhone(phone);
+            item.user_id = borrower_user?.id || "";
+            item.user.first_name = borrower_user?.first_name || "";
+            item.user.last_name = borrower_user?.last_name || "";
+            item.user.fullName = borrower_user?.fullName || "";
+            item.user.is_verified = borrower_user?.is_verified || false;
+            item.user.image = borrower_user?.image || "";
+    
+            // You can access the index using 'index' variable here
+          } else {
+            console.log(
+              `Index: ${item}, Invalid phone number: ${item.sapati.phone}`
+            );
+          }
+        }
+    
+        for (const item of lendings) {
+          const phone = parseInt(item.sapati.phone);
+          if (!isNaN(phone)) {
+            const borrower_user = await getUserByPhone(phone);
+            item.user_id = borrower_user?.id || "";
+            item.user.first_name = borrower_user?.first_name || "";
+            item.user.last_name = borrower_user?.last_name || "";
+            item.user.fullName = borrower_user?.fullName || "";
+            item.user.is_verified = borrower_user?.is_verified || false;
+            item.user.image = borrower_user?.image || "";
+    
+            // You can access the index using 'index' variable here
+          } else {
+            console.log(
+              `Index: ${item}, Invalid phone number: ${item.sapati.phone}`
+            );
+          }
+        }
+
+   
+    
+
+      const sapatiTaken=borrowings.sort(
+        (a, b) =>
+          new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+      ).map((item)=>(
+          {
+              user_id: item.user_id,
+              sapati_id: item.sapati_id,
+              first_name: item.user.first_name,
+              last_name: item.user.last_name,
+              fullName:item.user.fullName,
+              isverified:item.user.is_verified,
+              created_at:item.sapati.created_at,
+              status:"Borrowed",
+              sapati_status:item.sapati.sapati_satatus,
+              confirm_settlement:item.sapati.confirm_settlement,
+              amount:item.sapati.amount,
+              image:item.user.image,
+              remarks:item.sapati.remarks,
+              taken_date:item.sapati.taken_date,
+              return_date:item.sapati.return_date,
+              request_change_date:item.sapati.request_change_date,
+              changed_remarks:item.sapati.changed_remarks,
+              decline_reason:item.sapati.decline_reason,
+              change_reason:item.sapati.change_reason,
+              created_by:item.sapati.created_by,
+              currentUser:existingToken.user_id,
+              settled_date:item.sapati.settled_date,
+              
+
+          }
+      ))
+
+      const sapatiGiven=lendings.sort(
+        (a, b) =>
+          new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+      ).map((item)=>(
+          {
+              user_id: item.user_id,
+      sapati_id: item.sapati_id,
+      first_name: item.user.first_name,
+      last_name: item.user.last_name,
+              fullName:item.user.fullName,
+              isverified:item.user.is_verified,
+              created_at:item.sapati.created_at,
+              status:"Lent",
+              sapati_status:item.sapati.sapati_satatus,
+              confirm_settlement:item.sapati.confirm_settlement,
+              amount:item.sapati.amount,
+              image:item.user.image,
+              remarks:item.sapati.remarks,
+              taken_date:item.sapati.taken_date,
+            return_date:item.sapati.return_date,
+            changed_remarks:item.sapati.changed_remarks,
+            decline_reason:item.sapati.decline_reason,
+            change_reason:item.sapati.change_reason,
+            request_change_date:item.sapati.request_change_date,
+            created_user_name:item.sapati.created_user_name,
+            created_by:item.sapati.created_by,
+            currentUser:existingToken.user_id,
+            settled_date:item.sapati.settled_date,
+
+
+          }
+      ));
+
+      console.log(sapatiGiven)
+      console.log(sapatiTaken)
+
+      if(status=="activebook"){
+        const given=sapatiGiven.filter((item) =>(item.sapati_status=="PENDING"||item.sapati_status=="APPROVED"))
+        
+        const taken=sapatiTaken.filter((item) =>(item.sapati_status=="PENDING"||item.sapati_status=="APPROVED"))
+        data=[...given,...taken];
+        return NextResponse.json({message:"Successfully fetched transactions",data},{status:200})
+      }
+      if(status=="settled"){
+        const given=sapatiGiven.filter((item) =>(item.confirm_settlement==true))
+        const taken=sapatiTaken.filter((item) =>(item.confirm_settlement==true))
+        data=[...given,...taken];
+        return NextResponse.json({message:"Successfully fetched transactions",data},{status:200})
+      }
+
+      if(status=="rejected"){
+        const given=sapatiGiven.filter((item) =>(item.sapati_status=="DECLINED"))
+        const taken=sapatiTaken.filter((item) =>(item.sapati_status=="DECLINED"))
+        data=[...given,...taken];
+        return NextResponse.json({message:"Successfully fetched transactions",data},{status:200})
+      }
       }
 
     catch (error) {
-        console.log(error);
+        console.error(error);
         return NextResponse.json(
           { error: "Internal server error" },
           { status: 500 }
