5d2a3bd99764da28a11287b2dfbddab256c0f0a0
puru12 <puru.shiw2@gmail.com>
2024-05-08 11:05:46 +0700

    fixed me api

--- app/api/me/route.ts
+++ app/api/me/route.ts
@@ -68,83 +68,32 @@ export async function GET(req: NextRequest) {
         .map((item) => item.sapati.amount)
     );
     const balance = borrowings - lendings;
-    console.log(user.borrowings);
-    // const overallTransactions =
-    //   user.borrowings.filter((item) => item.sapati.sapati_satatus != "DECLINED")
-    //     .length +
-    //   user.lendings.filter((item) => item.sapati.sapati_satatus !== "DECLINED")
-    //     .length;
-
+    const overallTransactions =
+      user.borrowings.filter((item) => item.sapati.sapati_satatus != "DECLINED")
+        .length +
+      user.lendings.filter((item) => item.sapati.sapati_satatus !== "DECLINED")
+        .length;
     const takenTransactions = user.borrowings.filter(
       (item) =>
         item.sapati.sapati_satatus == "APPROVED" ||
         item.sapati.sapati_satatus == "SETTLED"
     );
-    console.log(user.lendings);
-    const uniqueLendings = new Map();
-
-    // Iterate through the data array and populate the uniqueEntriesMap
-    user.lendings.forEach((item) => {
-      const key = item.sapati.created_by + "-" + item.sapati.created_for;
-      uniqueLendings.set(key, item);
-    });
-
-    // Convert the map values back to an array to get the unique entries
-    const userLendings = Array.from(uniqueLendings.values());
-
-    console.log(userLendings);
-
-    const givenTransactions = userLendings.filter(
+    const givenTransactions = user.lendings.filter(
       (item) =>
         item.sapati.sapati_satatus == "APPROVED" ||
         item.sapati.sapati_satatus == "SETTLED"
     );
-    console.log(givenTransactions);
-    const pendingGiven = userLendings.filter(
+    const pendingGiven = user.lendings.filter(
       (item) =>
         item.sapati.sapati_satatus == "APPROVED" &&
         !item.sapati.confirm_settlement
     );
-
-    const uniqueBorrowings = new Map();
-
-    // Iterate through the data array and populate the uniqueEntriesMap
-    user.borrowings.forEach((item) => {
-      const key = item.sapati.created_by + "-" + item.sapati.created_for;
-      uniqueBorrowings.set(key, item);
-    });
-
-    // Convert the map values back to an array to get the unique entries
-    const userBorrowings = Array.from(uniqueBorrowings.values());
-
-    console.log(userBorrowings);
-    const pendingTaken = userBorrowings.filter(
+    const pendingTaken = user.borrowings.filter(
       (item) =>
         item.sapati.sapati_satatus == "APPROVED" &&
         !item.sapati.confirm_settlement
     );
     // const overallTransactions =givenTransactions.length+takenTransactions.length+pendingGiven.length+pendingTaken.length
-    const allTransactions = [...userLendings, ...userBorrowings];
-
-    // Create a map to store unique transactions
-    const uniqueTransactionKeys = new Map();
-
-    // Iterate through all transactions and populate the uniqueTransactionKeys map
-    allTransactions.forEach((item) => {
-      const key = `${item.sapati.created_by}-${item.sapati.created_for}-${item.sapati.type}`;
-      uniqueTransactionKeys.set(key, item);
-    });
-
-    // Create an array to store unique transactions
-    const uniqueTransactions = Array.from(uniqueTransactionKeys.values());
-
-    console.log(uniqueTransactions);
-
-    // const overallTransactions = userOverall.filter(
-    //   (item) => item.sapati.sapati_satatus != "DECLINED"
-    // ).length;
-
-    const overallTransactions = 122;
 
     let existingUser: ExtendedUser = (await getUserById(
       user.id
